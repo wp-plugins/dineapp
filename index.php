dc9cb452e1d5151c4cb7cc285c1415bc9425bcf0
dineapp <dineapp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-16 02:21:15 +0000

    update version to 1.1.2
    git-svn-id: https://plugins.svn.wordpress.org/dineapp/trunk@653282 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: DineApp
 Plugin URI: http://www.dineapp.com/
 Description: Restaurant online booking service.
-Version: 1.1.1
+Version: 1.1.2
 Author: DineApp Inc.
 Author URI: http://www.dineapp.com/
 */
@@ -15,12 +15,8 @@ add_action('admin_menu', array('DineApp', 'register_admin_panel'));
 
 // register activation & deactivation hook
 $pluginMainFile = WP_PLUGIN_DIR . '/dineapp/index.php';
-register_activation_hook( $pluginMainFile, function(){
-    DineApp::on_activated();
-});
-register_deactivation_hook($pluginMainFile, function(){
-    DineApp::on_deactivated();
-});
+register_activation_hook( $pluginMainFile, array('DineApp', 'on_activated'));
+register_deactivation_hook($pluginMainFile, array('DineApp', 'on_deactivated'));
 
 // register widget
 DineApp::register_sidebar_widget();
